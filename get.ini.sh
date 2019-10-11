fdef6025097017f20020e0079cd9aa294a4d28aa
Roman Lagutin <30324030+rlagutinhub@users.noreply.github.com>
2019-10-11 10:09:32 +0300

    mod get.ini.sh

--- get.ini.sh
+++ get.ini.sh
@@ -2,29 +2,32 @@
 
 # NAME:   GET.INI.SH
 # DESC:   PARS INI FILE
-# DATE:   10-10-2019
+# DATE:   11-10-2019
 # LANG:   BASH
 # AUTHOR: LAGUTIN R.A.
 # EMAIL:  RLAGUTIN@MTA4.RU
 
 # cat settings.ini
 # [Base]
-# DOMAIN_NAME=MTA4RU
-# ADMIN_NAME=AdminServer
-# ADMIN_LISTEN_PORT=7001
-# PRODUCTION_MODE=prod
-# ADMINISTRATION_PORT_ENABLED=true
-# ADMINISTRATION_PORT=9002
-# ADMIN_CONSOLE_ENABLED=true
-# DERBY_ENABLED=false
+# keys=base
+# base.domain_name=MTA4RU
+# base.admin_name=AdminServer
+# base.admin_listen_port=7001
+# base.production_mode=prod
+# base.administration_port_enabled=true
+# base.administration_port=9002
+# base.admin_console_enabled=true
+# base.derby_enabled=false
 
 # [Security]
-# username=weblogic
-# password=welcome1
+# keys=sec
+# sec.username=weblogic
+# sec.password=welcome1
 
 # [Java]
-# USER_MEM_ARGS=-Xms1024m -Xmx1024m -Djava.security.egd=file:/dev/./urandom
-# JAVA_OPTIONS=-Dweblogic.configuration.schemaValidationEnabled=false -Dfile.encoding=UTF-8 -Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=n -Djava.io.tmpdir=/tmp/
+# keys=java
+# java.user_mem_args=-Xms1024m -Xmx1024m -Djava.security.egd=file:/dev/./urandom
+# java.java_options=-Dweblogic.configuration.schemaValidationEnabled=false -Dfile.encoding=UTF-8 -Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=n -Djava.io.tmpdir=/tmp/
 
 FILE="settings.ini"
 SECTION="Base"
